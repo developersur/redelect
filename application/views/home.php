1cca747caf8ac4f0a903330f70ffd0e4bb3a4d24
Luis Matamala <luismatamala@live.com>
2018-06-30 19:05:10 -0400

    Minombre

--- application/views/home.php
+++ application/views/home.php
@@ -34,7 +34,7 @@
                         <div class="dropdown">
                             <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
                                 <i class="fa fa-shopping-cart"></i>
-                                <span>Your Cart</span>
+                                <span>Your CartLuis</span>
                                 <div class="qty">3</div>
                             </a>
                             <div class="cart-dropdown">
