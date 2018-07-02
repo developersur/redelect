ea074d01bfc8aa9f0c0a58e8623bf897bddc22fb
Luis Matamala <luismatamala@live.com>
2018-07-02 16:50:47 -0400

    Actualización de diseño

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -6,55 +6,39 @@
 				<div class="container">
 					<!-- row -->
 					<div class="row">
-						<div class="col-md-3 col-xs-6">
+						<div class="col-md-4 col-xs-6">
 							<div class="footer">
-								<h3 class="footer-title">aaaaaaaaaaaaaaaaa</h3>
+								<h3 class="footer-title">Nosotros</h3>
 								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
 								<ul class="footer-links">
-									<li><a href="#"><i class="fa fa-map-marker"></i>1734 Stonecoal Road</a></li>
-									<li><a href="#"><i class="fa fa-phone"></i>+021-95-51-84</a></li>
-									<li><a href="#"><i class="fa fa-envelope-o"></i>email@email.com</a></li>
+									<li><a href="#"><i class="fa fa-map-marker"></i>Barros Arana 492 oficina 78, Torre Ligure, Concepción</a></li>
+									<li><a href="#"><i class="fa fa-phone"></i>+56950077896</a></li>
+									<li><a href="#"><i class="fa fa-envelope-o"></i>whernandez.fobos@gmail.com</a></li>
 								</ul>
 							</div>
 						</div>
 
-						<div class="col-md-3 col-xs-6">
+						<div class="col-md-4 col-xs-6">
 							<div class="footer">
-								<h3 class="footer-title">Categories</h3>
+								<h3 class="footer-title">Categorias</h3>
 								<ul class="footer-links">
-									<li><a href="#">Hot deals</a></li>
-									<li><a href="#">Laptops</a></li>
-									<li><a href="#">Smartphones</a></li>
-									<li><a href="#">Cameras</a></li>
-									<li><a href="#">Accessories</a></li>
+									<li><a href="#">Fuerza</a></li>
+									<li><a href="#">Alumbrado</a></li>
+									<li><a href="#">Puntos de red</a></li>
+									<li><a href="#">Empalmes</a></li>
+									<li><a href="#">Servicios</a></li>
 								</ul>
 							</div>
 						</div>
 
 						<div class="clearfix visible-xs"></div>
 
-						<div class="col-md-3 col-xs-6">
+						<div class="col-md-4 col-xs-6">
 							<div class="footer">
-								<h3 class="footer-title">Information</h3>
+								<h3 class="footer-title">Información</h3>
 								<ul class="footer-links">
-									<li><a href="#">About Us</a></li>
-									<li><a href="#">Contact Us</a></li>
-									<li><a href="#">Privacy Policy</a></li>
-									<li><a href="#">Orders and Returns</a></li>
-									<li><a href="#">Terms & Conditions</a></li>
-								</ul>
-							</div>
-						</div>
-
-						<div class="col-md-3 col-xs-6">
-							<div class="footer">
-								<h3 class="footer-title">Service</h3>
-								<ul class="footer-links">
-									<li><a href="#">My Account</a></li>
-									<li><a href="#">View Cart</a></li>
-									<li><a href="#">Wishlist</a></li>
-									<li><a href="#">Track My Order</a></li>
-									<li><a href="#">Help</a></li>
+									<li><a href="#">Acerca de</a></li>
+									<li><a href="#">Contáctanos</a></li>
 								</ul>
 							</div>
 						</div>
@@ -97,7 +81,10 @@
 		<!-- jQuery Plugins -->
 		<script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
 		<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/slick.min.js"></script>
 		<script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/nouislider.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/jquery.zoom.min.js"></script>
 
 	</body>
 </html>
--- application/views/template/head.php
+++ application/views/template/head.php
@@ -4,9 +4,11 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<meta name="description" content="">
+  		<meta name="author" content="developersur">
 		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-		<title> OtraA</title>
+		<title>Redelect. Instalaciones electricas.</title>
 
 		<!-- Google font -->
 		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
@@ -19,6 +21,15 @@
 
 		<!-- Custom stlylesheet -->
 		<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css"/>
+
+		<!-- nouislider -->
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/css/nouislider.min.css"/>
+
+		<!-- -->
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/css/slick-theme.css"/>
+		
+		<!--  -->
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>assets/css/slick.css"/>
     </head>
     <body>
 		<!-- HEADER -->
@@ -37,3 +48,31 @@
 				</div>
 			</div>
 			<!-- /TOP HEADER -->
+
+			
+			<!-- MAIN HEADER -->
+			<div id="header">
+				<!-- container -->
+				<div class="container">
+					<!-- row -->
+					<div class="row">
+						<!-- LOGO -->
+						<div class="col-md-4">
+							<div class="header-logo">
+								<a href="#" class="logo">
+									<font size=10><img src="<?php echo base_url(); ?>assets/img/logo_redelect.jpeg" height="60" width="60" alt="logo">RedElect.</font>
+								</a>
+							</div>
+						</div>
+						<!-- /LOGO -->
+
+						<!-- SEARCH BAR -->
+						<div class="col-md-5">
+							<div class="header-search">
+								<form>
+									<input class="input" placeholder="¿Qué estás buscando?">
+									<button class="search-btn">Buscar</button>
+								</form>
+							</div>
+						</div>
+						<!-- /SEARCH BAR -->
