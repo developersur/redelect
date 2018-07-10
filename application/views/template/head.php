035dc088380c31085d40ed9152100beb1a52916b
Luis Matamala <luimatam@developersur.cl>
2018-07-10 11:45:29 -0400

    add whathelp

--- application/views/template/head.php
+++ application/views/template/head.php
@@ -52,6 +52,21 @@
 			gtag('config', 'UA-122092742-1');
 		</script>
 
+		<!-- WhatsHelp.io widget -->
+		<script type="text/javascript">
+			(function () {
+				var options = {
+					call_to_action: "Contactanos", // Call to action
+					button_color: "#129BF4", // Color of button
+					position: "right", // Position may be 'right' or 'left'
+				};
+				var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+				var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+				s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+				var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+			})();
+		</script>
+		<!-- /WhatsHelp.io widget -->
 
     </head>
     <body>
